9a04a1dad623325a0a5da89357997ca7baf9a743
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2014-12-18 17:22:29 -0500

    added database validations: no nilvalues for any fields in time_periods or hero_cards

--- db/migrate/20141218191012_create_hero_cards.rb
+++ db/migrate/20141218191012_create_hero_cards.rb
@@ -1,12 +1,12 @@
 class CreateHeroCards < ActiveRecord::Migration
   def change
     create_table :hero_cards do |t|
-      t.string :name
-      t.references :time_period, index: true
-      t.text :description
-      t.integer :strength
-      t.integer :intelligence
-      t.integer :charisma
+      t.string :name, :null => false
+      t.references :time_period, index: true, :null => false
+      t.text :description, :null => false
+      t.integer :strength, :null => false
+      t.integer :intelligence, :null => false
+      t.integer :charisma, :null => false
 
       t.timestamps
     end
