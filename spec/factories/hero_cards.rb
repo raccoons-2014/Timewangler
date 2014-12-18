71ab4c0cd01459b56394e6fe3b2f77bd8a4d3bbd
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-18 15:45:25 -0500

    Made small tests for hero_card and time_period specs. Added configuration to rails helper to use Factory Girl without calling Factory Girl explicitly every time.

--- spec/factories/hero_cards.rb
+++ spec/factories/hero_cards.rb
@@ -2,11 +2,11 @@
 
 FactoryGirl.define do
   factory :hero_card do
-    name Faker::Name.name
-    time_period rand(1..5)
-    description Faker::Lorem.sentence
-    strength rand(1..5)
-    intelligence rand(1..5)
-    charisma rand(1..5)
+    name { Faker::Name.name }
+    time_period_id { rand(1..5) }
+    description { Faker::Lorem.sentence }
+    strength { rand(1..5) }
+    intelligence { rand(1..5) }
+    charisma { rand(1..5) }
   end
 end
--- spec/models/hero_card_spec.rb
+++ spec/models/hero_card_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe HeroCard, :type => :model do
   let(:hero_card) {create(:hero_card)}
+
+  context "associations" do
+    it {  should belong_to :time_period }
+  end
+
 end
+
+
+
