b47bc03fb5efc8def7c1eccd2d6781c7f525ed0b
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-19 15:10:40 -0500

    Fixed bug in deck model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,6 +3,7 @@ class Deck < ActiveRecord::Base
   has_many :deck_card_relationships
   has_many :hero_cards, through: :deck_card_relationships
 
-def create_shuffled_deck
-  HeroCard.all.shuffle[1..15]
+  def create_shuffled_deck
+    HeroCard.all.shuffle[1..15]
+  end
 end
