e2df5f6d6eff18ec6f2f8d9ef6fa49bb34661f2f
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-19 02:11:48 -0500

    updated seed file to be more historically accurate

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
   TimePeriod.create(name: Faker::Lorem.word)
 end
 
-historical_figure_array = ["Abraham Lincoln", "Kanye West", "Confucius", "Socrates", "Genghis Khan", "Joan of Arc", "Julius Caesar", "Cleopatra", "Christopher Columbushistorical", "William Shakespeare", "Princess Diana", "Venus Williams", "Stephen Hawking", "Martin Luther King", "Nelson Mandela", "Barack Obama"]
+historical_figure_array = ["Abraham Lincoln", "Kanye West", "Confucius", "Socrates", "Genghis Khan", "Joan of Arc", "Julius Caesar", "Cleopatra", "Christopher Columbushistorical", "William Shakespeare", "Princess Diana", "Venus Williams", "Stephen Hawking", "Martin Luther King", "Nelson Mandela", "Barack Obama", "Grace Hopper", "Napoleon", "Mao Tse-tung", "George Washington", "Spartacus", "Pocahantas", "Queen Elizabeth I", "Marie Curie", "Amelia Earhart", "Margaret Thatcher", "Kim Jong Il", "Joseph Stalin", "Mark Twain", "Friedrich Nietzsche"]
 
 30.times do
-  HeroCard.create(name: Faker::Name.name, time_period_id: rand(1..5), description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
+  HeroCard.create(name: historical_figure_array.pop, time_period_id: rand(1..5), description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
 end
 
 
