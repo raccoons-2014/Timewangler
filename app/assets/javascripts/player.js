7a2eb0db737305306e5fcb7d233ae524d67c22ce
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-19 10:27:39 -0500

    Added .DS_Store to .gitignore, refactored player.js

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,27 +1,17 @@
-// Player Health
-// Incomplete
 var Player = function(deck, health) {
   this.health = health,
   this.deck = deck,
-  this.hand = [],
-  this.drawHand = function() {
-    // Pops cards from deck into this.hand until there's 6 cards.
-    while (this.hand.length < 6) {
+  this.hand = []
+  };
+
+Player.prototype.drawHand = function() {
+  while (this.hand.length < 6) {
       console.log(this.deck);
       this.hand.push(this.deck.pop());
     };
-  },
+};
 
-  this.playCard = function() {
-   return this.hand.pop();
-  }
-  // this.hand = function() {
-  //   $.ajax ({
-  //     url: "game.html",
-  //     dataType: "JSON"
-  //   }).done(function(content) {
-  //     return content;
-  //   });
-  };
-
+Player.prototype.playCard = function() {
+  return this.hand.pop();
+};
 
