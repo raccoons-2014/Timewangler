27e278aa8576abddc27a043f7d9f3fb99f5f233f
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-19 13:28:57 -0500

    Updated Users_Controller_spec
    X'd out the test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe UsersController, :type => :controller do
-#     context "while filling out the fields" do
-#       describe "user#create" do
-#         it "allows a user to be created when the from username params" do
-#           expect{
-#             User.create(username: "lulu")
-#           }.to change(User, :count)
-#         end
-#       end
-#     end
-# end
+RSpec.describe UsersController, :type => :controller do
+    context "while filling out the fields" do
+      describe "user#create" do
+        xit "allows a user to be created when the from username params" do
+          expect{
+            User.create(username: "lulu")
+          }.to change(User, :count)
+        end
+      end
+    end
+end
 
