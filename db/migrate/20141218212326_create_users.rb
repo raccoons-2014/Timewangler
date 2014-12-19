855f93afc1da2031e3b0ccd7b360c753518f9325
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-18 20:58:34 -0500

    add database constraints on email and username

--- db/migrate/20141218212326_create_users.rb
+++ db/migrate/20141218212326_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, :required
-      t.string :email, :required
+      t.string :username, :null => false
+      t.string :email, :null => false
       t.string :password_digest
 
       t.timestamps
