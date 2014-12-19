5b86b77dac4efa812f4e5a06e2f05997c5d598eb
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-19 12:39:12 -0500

    add unfinished resolveRound function

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -12,3 +12,29 @@ Game.prototype.isWon = function() {
     return false;
   };
 }
+
+Game.prototype.resolveRound = function() {
+  var playerOneMove = this.round.playerOneCard;
+  var playerTwoMove = this.round.playerTwoCard;
+
+  if ( playerOneMove && playerTwoMove ) {
+    this.sumStats(playerOneMove)
+    this.sumStats(playerTwoMove)
+  }
+  //
+  var playerOneMove = this.playerOne.round;
+  var playerTwoMove = this.playerTwo.round;
+
+  if ( this.playerOne.round && this.playerTwo.round ) {
+    this.sumStats(this.playerOne.round)
+    this.sumStats(this.playerTwo.round)
+    Math.max(this.sumStats(playerOneMove), this.sumStats(playerTwoMove))
+  }
+  // UNFINISHED
+
+
+}
+
+Game.prototype.sumStats = function(card) {
+  return card.charisma + card.intelligence + card.strength;
+}
