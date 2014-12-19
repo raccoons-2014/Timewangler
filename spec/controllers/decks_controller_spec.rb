b4319f8ff7317fdf9af9b00717925f5579625ab5
William Storey <wcstorey@gmail.com>
2014-12-19 15:33:03 -0500

    created decks_controller tests to ensure deck1 and deck2 actions return json objects

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe DecksController, :type => :controller do
+  render_views # <-- You need this to test your json response!
+
+  context "when accessing the deck1 route" do
+    describe "user#deck1" do
+      it "returns a json object" do
+        post :deck1
+        expect {JSON.parse(response.body)}.to_not raise_error
+      end
+    end
+  end
+
+  context "when accessing the deck2 route" do
+    describe "user#deck2" do
+      it "returns a json object" do
+        post :deck2
+        expect {JSON.parse(response.body)}.to_not raise_error
+      end
+    end
+  end
 
 end
