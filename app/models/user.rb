df0ab3ddd560b77aed4996cc13827e8092cdf6ba
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-18 21:05:44 -0500

    remove email validation on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
 
   has_many :games, foreign_key: :player_one_id
   has_many :games, foreign_key: :player_two_id
-
-  validates :email, :format => { :with => /\A\w+[@]\w+[.]\w*[.]*\w{2,}/}
 end
