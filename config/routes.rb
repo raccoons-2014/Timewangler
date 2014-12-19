e68e35dbab670b7a56198677034ff2640ab1dae6
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-18 21:09:59 -0500

    renamed somethings, start building routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -53,8 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to => "games#new"
+  resources :games
+  resources :decks
+  resources :hero_cards
 
-  resources :deck
-  resources :hero_card
-  
 end
--- spec/controllers/deck_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DeckController, :type => :controller do
-
-end
