c85c8e3a96c8906b5c090853f9d3b5f410054fc1
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-18 21:20:18 -0500

    took out user's email and passwords for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # has_secure_password
 
   has_many :games, foreign_key: :player_one_id
   has_many :games, foreign_key: :player_two_id
--- db/migrate/20141218212326_create_users.rb
+++ db/migrate/20141218212326_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, :null => false
-      t.string :email
-      t.string :password_digest
+      # t.string :email
+      # t.string :password_digest
 
       t.timestamps
     end
