ac8abcdead0033222ad0024df6fdc6375891248d
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2014-12-18 19:29:56 -0500

    reset the javascript to a blank slate

--- app/assets/javascripts/servercomm.js
+++
@@ -1,5 +0,0 @@
-var request = new XMLHttpRequest();
-request.onload = callback;
-request.open("post", "http://localhost:3000/request");
-request.setRequestHeader("Content-Type", "application/json");
-request.send('{"my_data":"back to basics"}');
