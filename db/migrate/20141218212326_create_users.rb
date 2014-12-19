aa01286ad8ab2e5d6a17460276681d90b3b69097
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-18 21:04:24 -0500

    remove the db check on email

--- db/migrate/20141218212326_create_users.rb
+++ db/migrate/20141218212326_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, :null => false
-      t.string :email, :null => false
+      t.string :email
       t.string :password_digest
 
       t.timestamps
