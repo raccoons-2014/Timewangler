b517a72db0b9a810154d553b0e20c265d9f48ca5
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-19 01:38:02 -0500

    Passed 3 Jasmine Tests: Player objects have health property, contain deck objects, and possess a drawHand function that pops elements from Deck array and pushes them into Hand array.

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,15 +1,22 @@
 // Player Health
 // Incomplete
-var Player = function() {
+var Player = function(deck, player) {
   this.health = 30,
-  this.hand = function() {
-    $.ajax ({
-      url: "game.html",
-      dataType: "JSON"
-    }).done(function(content) {
-      return content;
-    });
+  this.deck = deck,
+  this.hand = [],
+  this.drawHand = function() {
+    // Pops cards from deck into this.hand until there's 6 cards.
+    while (this.hand.length < 6) {
+      this.hand.push(this.deck.pop());
+    };
   }
-}
+  // this.hand = function() {
+  //   $.ajax ({
+  //     url: "game.html",
+  //     dataType: "JSON"
+  //   }).done(function(content) {
+  //     return content;
+  //   });
+  };
 
 
--- spec/javascripts/PlayerSpec.js
+++ spec/javascripts/PlayerSpec.js
@@ -1,13 +1,22 @@
 describe("Player", function() {
   var player;
+  var deck = ["Abraham Lincoln", "Albert Einstein", "Mahatma Gandhi", "Pootie Tang", "Bruce Lee", "Florida Man"];
 
   beforeEach(function() {
-    player = new Player(30);
-
+    player = new Player(deck, 30);
   });
 
   it("has a health property", function() {
     expect(player.health).toEqual(30);
+  });
+
+  it("has a deck", function() {
+    expect(typeof player.deck).toEqual("object");
+  });
+
+  it("has a deck that contains 6 objects.", function() {
+    player.drawHand();
+    expect(player.hand.length).toEqual(6);
   });
 
 });
