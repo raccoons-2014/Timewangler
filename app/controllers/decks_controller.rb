170ad4a2acc9198d7a6dfbe939039cece8e35189
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-18 23:43:37 -0500

    commented out notes for later

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -5,13 +5,13 @@ class DecksController < ApplicationController
 
 
 #Make a method to replenish the hand to have 6 cards at the beginning of each turn
-  @player_1 = User.find(1)
-  @player_2 = User.find(2)
+  # @player_1 = User.find(1)
+  # @player_2 = User.find(2)
 
   def prepare_player_deck(player)
     # deck = Deck.find(player.id)
-    deck = Deck.find(user: player)
-    deck = deck.shuffle
+    # deck = Deck.find(user: player)
+    # deck = deck.shuffle
   end
 
 
