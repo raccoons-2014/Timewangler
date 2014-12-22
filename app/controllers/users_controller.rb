648e0deede419e821c00b345b1ccdc6c18b47445
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-21 19:44:37 -0500

    fix redirect and add error handling for new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
 
     if @user.save
       login(@user)
-      redirect_to user_show
+      redirect_to user_path(@user)
     else
+      flash[:error] = @user.errors.full_messages.join(", ")
       redirect_to new_user_path
     end
   end
