0b25a09cdc87f7628a72c5033eb8518ae5a8358c
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-21 19:04:07 -0500

    add migration to update users table with new fields for password and email

---
+++ db/migrate/20141221222215_update_users_table_with_password_and_email.rb
@@ -0,0 +1,6 @@
+class UpdateUsersTableWithPasswordAndEmail < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+    add_column :users, :email, :string
+  end
+end
