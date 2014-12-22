971762b9d961e6ce787f0f74eb1aec8caa06f39b
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-21 19:00:58 -0500

    update Users table to include a password and email field

--- db/schema.rb
+++ db/schema.rb
@@ -63,9 +63,11 @@ ActiveRecord::Schema.define(version: 20141218235231) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username",   null: false
+    t.string   "username",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "email"
   end
 
 end
