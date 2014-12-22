e05781e024708df934a0250144a519e5b9af979b
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-21 19:55:44 -0500

    add password_confirmation to the accepted params list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :password, :email)
+      params.require(:user).permit(:username, :password, :password_confirmation, :email)
     end
 
 end
