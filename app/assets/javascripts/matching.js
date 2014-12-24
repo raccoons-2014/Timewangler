bd262813da97f54561a88ee2364f4c32c898eaac
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-23 21:45:08 -0500

    add javascript for polling the server on the status of player matches and redirect if a sucessful match is found

---
+++ app/assets/javascripts/matching.js
@@ -0,0 +1,37 @@
+function getGameId() {
+  var fullUrl = document.URL;
+  var urlArray = fullUrl.split('/');
+  return urlArray[4];
+}
+
+function checkMatch() {
+  setTimeout(function() {
+    console.log("Waiting for another player to join...");
+
+    $.ajax({
+        url: "/games/" + getGameId() + "/status",
+        method: "GET",
+        dataType: "JSON",
+        success: function(response) {
+          console.log(response);
+          if (response) {
+            window.location.replace(response.url);
+          } else {
+            checkMatch();
+          }
+        },
+        error: function(response) {
+          console.log("ERROR:");
+          console.log(response);
+        }
+      });
+
+  }, 1000)
+}
+
+$(document).ready(function() {
+  if ($('#match').length > 0) {
+    console.log('matches loaded');
+    checkMatch();
+  };
+})
