255efdc6adf5f8941789bb8f7a3e995e1fc447c1
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-24 17:01:07 -0500

    write tests for destroy action in GamesController

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, :type => :controller do
+  describe "games#destroy" do
+    before(:each) { @game = create(:game) }
 
+    it 'should decrease the count of existing games by one after calling the destroy action' do
+    expect{
+      Game.destroy(@game)
+    }.to change(Game, :count)
+    end
+
+    it 'should not include the game object in the database after it has been destroyed' do
+      Game.destroy(@game)
+      expect{ Game.find(@game.id) }.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
 end
