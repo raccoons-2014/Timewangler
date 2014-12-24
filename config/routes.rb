07fd797f8f05911b025e7e70b0ef2ad9e3fda201
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-23 21:44:08 -0500

    update routes to refer to support login path and url helpers

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rails.application.routes.draw do
 
   root :to => "pages#home"
 
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
 
