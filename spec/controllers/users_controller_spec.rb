fd84dc9575907f3086d8431d183029a28a05f1ed
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-24 20:33:50 -0500

    add tests to ensure that the Users#profile action works correctly

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,17 +21,16 @@ RSpec.describe UsersController, :type => :controller do
     end
   end
 
-      it "does not create a user when passed an empty password" do
-        expect{
-          User.create(email: "fake@gmail.com", username: "lulu", password: "")
-        }.to_not change(User, :count)
-      end
+  describe "user#profile" do
+    it 'should direct a logged-in user to their personal profile page' do
+      user = create(:user)
+      session[:user_id] = user.id
 
-      it "does not create a user when passed an empty username" do
-        expect{
-          User.create(email: "fake@gmail.com", username: "", password: "dfasdfasdf")
-        }.to_not change(User, :count)
-      end
+      expect(get :profile).to render_template :profile
+    end
+
+    it 'should re-direct any requests that are not logged in to the homepage' do
+      expect(get :profile).to redirect_to root_path
     end
   end
 end
