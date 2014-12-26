3f82f688ffc4e7fdbe6abfab6f07c504c417267b
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-26 16:18:50 -0500

    add initialize method to GameEngine::Card class, add max_stat method to Card class

--- app/game/game_engine/card.rb
+++ app/game/game_engine/card.rb
@@ -1,5 +1,21 @@
 module GameEngine
   class Card
-    #..
+    attr_reader :id, :card_type, :name, :description, :strength, :intelligence, :charisma, :time_period
+
+    def initialize(card_data)
+      @id = card_data.id
+      @type = card_data.card_type
+      @name = card_data.name
+      @description = card_data.description
+      @strength = card_data.strength
+      @intelligence = card_data.intelligence
+      @charisma = card_data.charisma
+      @time_period = card_data.time_period
+    end
+
+    def max_stat
+      [strength,intelligence,charisma].max
+    end
+
   end
 end
