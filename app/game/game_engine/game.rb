eb48c5e05c048a0d01ead9b63158cb7467958b31
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-26 17:03:45 -0500

    add Round object

--- app/game/game_engine/game.rb
+++ app/game/game_engine/game.rb
@@ -1,5 +1,11 @@
 module GameEngine
   class Game
-    #..
+
+    def initialize(who_knows)
+      @player_one_id = nil
+      @player_one_points = nil
+      @player_two_id = nil
+      @player_two_points = nil
+    end
   end
 end
