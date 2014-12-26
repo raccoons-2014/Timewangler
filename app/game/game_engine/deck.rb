947aaca9a62f9f8232211bece9a482bdff40b9d4
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-26 16:40:45 -0500

    add Deck class to game engine, write shuffle method for the class

--- app/game/game_engine/deck.rb
+++ app/game/game_engine/deck.rb
@@ -1,5 +1,12 @@
 module GameEngine
   class Deck
-    #..
+    attr_reader :list
+    def initialize(cards)
+      @list = cards.map { |card| GameEngine::Card.new(card) }
+    end
+
+    def shuffle
+      list.shuffle!
+    end
   end
 end
