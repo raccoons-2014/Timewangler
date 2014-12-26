79ba6023a148b09273721f49142274808e8c03d5
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-26 17:29:07 -0500

    add GameEngine::Game class

--- app/game/game_engine/game.rb
+++ app/game/game_engine/game.rb
@@ -1,11 +1,14 @@
 module GameEngine
   class Game
 
-    def initialize(who_knows)
-      @player_one_id = nil
-      @player_one_points = nil
-      @player_two_id = nil
-      @player_two_points = nil
+    def initialize(game_data, starting_points)
+      player_one_id = game_data.player_one_id
+      player_one_cards = game_data.player_one.deck.cards
+      player_one_points = starting_points
+
+      player_two_id = game_data.player_two_id
+      player_two_cards = game_data.player_two.deck.cards
+      player_two_points = starting_points
     end
   end
 end
