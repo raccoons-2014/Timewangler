42a33bf1658a1eb6d2d9d1b4b0ebc8fd72125b00
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-28 18:32:59 -0500

    refactoring game engine game

--- app/game/game_engine/game.rb
+++ app/game/game_engine/game.rb
@@ -2,7 +2,7 @@ module GameEngine
   GAME_RULES = { starting_points: 30, hand_size: 6 }
 
   class Game
-    attr_reader :id, :player_one_id, :player_one_points, :player_one_deck, :player_one_hand, :player_two_id, :player_two_points, :player_two_deck, :player_two_hand
+    attr_reader :id, :player_one_points, :player_one_deck, :player_one_hand, :player_two_points, :player_two_deck, :player_two_hand, :player_one, :player_two
 
     def initialize(game_data)
       @game_data = game_data
@@ -10,12 +10,10 @@ module GameEngine
       @player_one = GameEngine::Player.new(game_data.player_one)
       @player_two = GameEngine::Player.new(game_data.player_two)
 
-      @player_one_id = game_data.player_one_id
       @player_one_points = GAME_RULES[:starting_points]
       @player_one_deck = GameEngine::Deck.new(game_data.player_one.deck)
       @player_one_hand = []
 
-      @player_two_id = game_data.player_two_id
       @player_two_points = GAME_RULES[:starting_points]
       @player_two_deck = GameEngine::Deck.new(game_data.player_two.deck)
       @player_two_hand = []
@@ -24,18 +22,18 @@ module GameEngine
 
 
     def deal_cards
-      until player_one_hand.size == GAME_RULES[:hand_size]
-        @player_one_hand << player_one_deck.list.pop
+      until player_one.hand.size == GAME_RULES[:hand_size]
+        player_one.hand << player_one.deck.list.pop
       end
 
-      until player_two_hand.size == GAME_RULES[:hand_size]
-        @player_two_hand << player_two_deck.list.pop
+      until player_two.hand.size == GAME_RULES[:hand_size]
+        player_two.hand << player_two.deck.list.pop
       end
     end
 
 
     def play_round
-      parser = Parser.new(@player_one_deck, @player_one_hand, @player_two_deck, @player_two_hand, @game_data)
+      parser = Parser.new(@player_one_deck, @player_one_hand, @player_two_deck, @player_two_hand, @game_data, player_one, player_two, player_one_points, player_two_points)
       parser.save_round
     end
 
