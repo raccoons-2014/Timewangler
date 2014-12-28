1b57bf64b560c370102bd952855e63f111caff94
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-28 18:34:59 -0500

    add times for different phases to the GAME_RULES hash

--- app/game/game_engine/game.rb
+++ app/game/game_engine/game.rb
@@ -1,5 +1,5 @@
 module GameEngine
-  GAME_RULES = { starting_points: 30, hand_size: 6 }
+  GAME_RULES = { starting_points: 30, hand_size: 6, setup_time: 10, move_time: 30, resolution_time: 5 }
 
   class Game
     attr_reader :id, :player_one, :player_two
