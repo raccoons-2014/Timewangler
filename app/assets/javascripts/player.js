2bb98a11e101140a971156b673b881e710d64137
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-27 19:16:50 -0500

    comment out some of the old Javascript code temporarily

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,15 +1,15 @@
-var Player = function(deck, health) {
-  this.health = health,
-  this.deck = deck,
-  this.hand = []
-  };
-
-Player.prototype.drawHand = function() {
-  while (this.hand.length < 6) {
-      this.hand.push(this.deck.pop());
-    };
-};
-
-Player.prototype.playCard = function() {
-  return this.hand.pop();
-};
+// var Player = function(deck, health) {
+//   this.health = health,
+//   this.deck = deck,
+//   this.hand = []
+//   };
+//
+// Player.prototype.drawHand = function() {
+//   while (this.hand.length < 6) {
+//       this.hand.push(this.deck.pop());
+//     };
+// };
+//
+// Player.prototype.playCard = function() {
+//   return this.hand.pop();
+// };
