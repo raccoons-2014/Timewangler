4dd8579cd5b7e980dc9c8a60ca8251f500348c59
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-30 16:45:21 -0500

    remove timestamp from the game object generated by GameEngine::IO class

--- app/game/game_engine/io.rb
+++ app/game/game_engine/io.rb
@@ -1,13 +1,12 @@
 module GameEngine
   module IO
     def self.output_player_data(game_state, player_id)
-      timestamp = Time.now
 
       case player_id
       when game_state.player_one.id
-        return { player_hand: game_state.player_one.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, request_time: timestamp }
+        return { player_hand: game_state.player_one.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase }
       when game_state.player_two.id
-        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, request_time: timestamp }
+        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase }
       end
     end
   end
