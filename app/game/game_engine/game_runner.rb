9457eb25a2120487c829f68dfd89938cda029b39
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-30 18:47:34 -0500

    update #resolve_round method to work inside GameEngine::GameRunner

--- app/game/game_engine/game_runner.rb
+++ app/game/game_engine/game_runner.rb
@@ -5,35 +5,35 @@ module GameEngine
 
       if played_cards[0].nil? || played_cards[1].nil?
         if played_cards[0].nil? && played_cards[1].nil?
-          player_one.selection = []
-          player_two.selection = []
+          game.player_one.selection = []
+          game.player_two.selection = []
           # return nil
         elsif played_cards[0].nil?
-          player_one.points -= played_cards[1].max_stat
-          player_two.points += played_cards[1].max_stat
-          player_one.selection = []
-          player_two.selection = []
+          game.player_one.points -= played_cards[1].max_stat
+          game.player_two.points += played_cards[1].max_stat
+          game.player_one.selection = []
+          game.player_two.selection = []
         else
-          player_two.points -= played_cards[1].max_stat
-          player_one.points += played_cards[1].max_stat
-          player_one.selection = []
-          player_two.selection = []
+          game.player_two.points -= played_cards[1].max_stat
+          game.player_one.points += played_cards[1].max_stat
+          game.player_one.selection = []
+          game.player_two.selection = []
         end
       elsif played_cards[0].max_stat > played_cards[1].max_stat
         damage = (played_cards[0].max_stat - played_cards[1].max_stat).abs
 
-        player_two.points -= damage
-        player_one.points += damage
-        player_one.selection = []
-        player_two.selection = []
+        game.player_two.points -= damage
+        game.player_one.points += damage
+        game.player_one.selection = []
+        game.player_two.selection = []
       else
         damage = (played_cards[0].max_stat - played_cards[1].max_stat).abs
         # return nil if damage == 0
 
-        player_one.points -= damage
-        player_two.points += damage
-        player_one.selection = []
-        player_two.selection = []
+        game.player_one.points -= damage
+        game.player_two.points += damage
+        game.player_one.selection = []
+        game.player_two.selection = []
       end
     end
 
