bcc265ff255baea18bc15c2c74fc76fbdb9c62b7
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-30 16:28:33 -0500

    throw out GameEngine::Controller code because it was bad

--- app/game/game_engine/controller.rb
+++ app/game/game_engine/controller.rb
@@ -1,40 +1,4 @@
 module GameEngine
   module Controller
-    def self.advance_game(game_data, player_id)
-      game_state = GameEngine::Cache.fetch_game_state(game_data)
-      current_time = Time.now
-
-      case game_state.phase
-      when :setup
-        if current_time - game_state.time >= GameEngine::GAME_RULES[:setup_time]
-          game_state.phase = :move
-          GameEngine::Cache.save_game_state(game_state)
-          GameEngine::IO.output_player_data(game_state, player_id)
-        else
-          GameEngine::IO.output_player_data(game_state, player_id)
-        end
-      when :move
-        game_state.deal_cards
-
-        if current_time - game_state.time >= GameEngine::GAME_RULES[:move_time]
-          game_state.phase = :resolution
-          GameEngine::Cache.save_game_state(game_state)
-          GameEngine::IO.output_player_data(game_state, player_id)
-        else
-          GameEngine::Cache.save_game_state(game_state)
-          GameEngine::IO.output_player_data(game_state, player_id)
-        end
-      when :resolution
-        # Kinda broken, one client stuck in resolution phase forever while second never hits, or more likely they're flipping the phases back and forth between move and resolution.
-        if current_time - game_state.time >= GameEngine::GAME_RULES[:resolution_time]
-          game_state.phase = :move
-          game_state.round += 1
-          GameEngine::Cache.save_game_state(game_state)
-          GameEngine::IO.output_player_data(game_state, player_id)
-        else
-          GameEngine::IO.output_player_data(game_state, player_id)
-        end
-      end
-    end
   end
 end
