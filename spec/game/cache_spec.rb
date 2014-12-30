2c8cf8abc669314b1fe4fdb95d11720470cafda5
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-30 16:33:35 -0500

    break out game_engine_spec into separate spec files

---
+++ spec/game/cache_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "GameEngine::Cache" do
+  let(:game_data) { game_data = create(:game) }
+
+  let(:game) do
+    game_data.player_one.deck = create(:deck)
+    game_data.player_two.deck = create(:deck)
+
+    GameEngine::Game.new(game_data)
+  end
+
+  describe '#save_game_state' do
+    it 'does not return false after saving game data' do
+      expect(GameEngine::Cache.save_game_state(game)).to_not eq false
+    end
+  end
+
+  describe '#fetch_game_state' do
+    it 'returns the correct object' do
+      GameEngine::Cache.save_game_state(game)
+      expect(GameEngine::Cache.fetch_game_state(game_data).id).to eq game.id
+    end
+  end
+
+  describe '#clear_game_state' do
+    it 'deletes the correct object from the cache' do
+      GameEngine::Cache.save_game_state(game)
+      expect(GameEngine::Cache.fetch_game_state(game_data)).to_not eq nil
+      GameEngine::Cache.clear_game_state(game_data)
+      expect(GameEngine::Cache.fetch_game_state(game_data)).to eq nil
+    end
+  end
+end
