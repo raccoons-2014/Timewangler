ed97799f56d896b4bd5e39fd85d6abd31650b9ed
Matthew D. Huff <matthewhuff89@gmail.com>
2014-12-31 14:59:06 -0500

    Working on CSS for cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ end
 historical_figure_array = ["Abraham Lincoln", "Kanye West", "Confucius", "Socrates", "Genghis Khan", "Joan of Arc", "Julius Caesar", "Cleopatra", "Christopher Columbus", "William Shakespeare", "Princess Diana", "Venus Williams", "Stephen Hawking", "Martin Luther King", "Nelson Mandela", "Barack Obama", "Grace Hopper", "Napoleon", "Mao Tse-tung", "George Washington", "Spartacus", "Pocahantas", "Queen Elizabeth I", "Marie Curie", "Amelia Earhart", "Margaret Thatcher", "Kim Jong Il", "Joseph Stalin", "Mark Twain", "Friedrich Nietzsche"]
 
 30.times do
-  Card.create(card_type: "Murok", name: historical_figure_array.pop, description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
+  Card.create(card_type: "Hero", name: historical_figure_array.pop, description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
 end
 
 
@@ -19,3 +19,8 @@ end
 Card.where(:id => 16..30).each do |hero|
 	deck2.deck_card_relationships.create(card: hero)
 end
+
+# Template for Cards
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: )
+
+Card.create(card_type: "Hero", name:"Matthew D. Huff", description:"Get You Some..." , strength: 10 , intelligence: 10, charisma: 10, picture_url: "murok_face.jpeg")
