cb3d4275fe0329f448c0b49f8aa24457306a8984
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-31 16:00:57 -0500

    write code to create dummy objects for testing GameEngine::Controller

--- spec/game/controller_spec.rb
+++ spec/game/controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 describe 'GameEngine::Controller' do
-  #..
+  describe '#advance_game' do
+    before(:each) do
+      game_db_model = create(:game)
+      game_db_model.player_one.deck = create(:deck)
+      game_db_model.player_two.deck = create(:deck)
+
+      game = GameEngine::Game.new(game_db_model)
+      @game_setup = game.dup; @game_setup.phase = :setup
+      @game_move = game.dup; @game_setup.phase = :move
+      @game_resolution = game.dup; @game_setup.phase = :resolution
+
+    end
+
+    xit 'resolve a move phase correctly' do
+      #..
+    end
+  end
 end
