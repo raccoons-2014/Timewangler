793081cb0e7afc1f38bb6786dbad885f39f32495
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2015-01-02 18:19:48 -0500

    updating seed file to have seeds for better historical figure cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,189 @@
-5.times do
-  TimePeriod.create(name: Faker::Lorem.word)
-end
+# Template for Cards
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
 
-historical_figure_array = ["Abraham Lincoln", "Kanye West", "Confucius", "Socrates", "Genghis Khan", "Joan of Arc", "Julius Caesar", "Cleopatra", "Christopher Columbus", "William Shakespeare", "Princess Diana", "Venus Williams", "Stephen Hawking", "Martin Luther King", "Nelson Mandela", "Barack Obama", "Grace Hopper", "Napoleon", "Mao Tse-tung", "George Washington", "Spartacus", "Pocahantas", "Queen Elizabeth I", "Marie Curie", "Amelia Earhart", "Margaret Thatcher", "Kim Jong Il", "Joseph Stalin", "Mark Twain", "Friedrich Nietzsche"]
 
-30.times do
-  Card.create(card_type: "Hero", name: historical_figure_array.pop, description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
-end
+## ---------------------------------
+## CLASSICAL
+## ---------------------------------
+
+TimePeriod.create(name: "Classical Period")
+
+Card.create(card_type: "Hero", name: "Cleopatra", description: "Last active pharaoh of Ancient Egypt.", strength: 4, intelligence: 8, charisma: 8, picture_url: "http://thedeepestred.files.wordpress.com/2012/12/cleopatra.jpg")
+
+Card.create(card_type: "Hero", name: "Mark Antony", description: "A Roman politician and general who played a critical role in the transformation of the Roman Republic from an oligarchy into the autocratic Roman Empire.", strength: 8, intelligence: 8, charisma: 7, picture_url: "http://media-2.web.britannica.com/eb-media/21/13421-004-A2A61DF4.jpg")
+
+Card.create(card_type: "Hero", name: "Julius Caesar", description: "A Roman general, statesman, Consul, and notable author of Latin prose. He played a critical role in the events that led to the demise of the Roman Republic and the rise of the Roman Empire.", strength: 7, intelligence: 7, charisma: 9, picture_url: "http://a2.files.biography.com/image/upload/c_fill,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNDQxMTExMDU0.jpg")
+
+Card.create(card_type: "Hero", name: "Socrates", description: "A classical Greek philosopher credited as one of the founders of Western philosophy.", strength: 2, intelligence: 10, charisma: 4, picture_url: "http://philosophyforchange.files.wordpress.com/2012/04/socrates1.jpg")
+
+Card.create(card_type: "Hero", name: "Aristotle", description: "A Greek philosopher and scientist born in Stagira of Chalkidiki, next to the Macedonian Kingdom in the north part of the Greek world.", strength: 2, intelligence: 9, charisma: 5, picture_url: "http://media-1.web.britannica.com/eb-media/84/87984-004-5ADE9ACA.jpg")
+
+Card.create(card_type: "Hero", name: "Plato", description: "Plato was a philosopher, as well as mathematician, in Classical Greece, and an influential figure in philosophy, central in Western philosophy.", strength: 3, intelligence: 10, charisma: 6, picture_url: "http://scienceworld.wolfram.com/biography/pics/Plato.jpg")
+
+Card.create(card_type: "Hero", name: "Homer", description: "Best known as the author of the Iliad and the Odyssey. He was believed by the ancient Greeks to have been the first and greatest of the epic poets.", strength: 4, intelligence: 9, charisma: 7, picture_url: "http://upload.wikimedia.org/wikipedia/commons/1/1c/Homer_British_Museum.jpg")
+
+Card.create(card_type: "Hero", name: "Nero", description: "Roman Emperor from 54 to 68, and the last in the Julio-Claudian dynasty. Focused much of his attention on diplomacy, trade and enhancing the cultural life of the Empire. He ordered theatres built and promoted athletic games.", strength: 6, intelligence: 6, charisma: 8, picture_url: "http://upload.wikimedia.org/wikipedia/commons/1/1c/Homer_British_Museum.jpg")
+
+
+
+## ---------------------------------
+## MEDEIVAL
+## ---------------------------------
+
+TimePeriod.create(name: "Medieval Period")
+
+Card.create(card_type: "Hero", name: "Genghis Khan", description: "founder and Great Khan of the Mongol Empire, which became the largest contiguous empire in history after his demise. Totally ravaged China.", strength: 10, intelligence: 7, charisma: 9, picture_url: "http://upload.wikimedia.org/wikipedia/commons/4/49/Ogadai_Khan.jpg")
+
+Card.create(card_type: "Hero", name: "Joan of Arc", description: "a heroine of France and a Roman Catholic saint. Was burned at the stake at about nineteen years of age.", strength: 10, intelligence: 8, charisma: 5, picture_url: "http://www.history.com/news/wp-content/uploads/2012/01/joan-of-arc.jpg")
+
+
+
+## ---------------------------------
+## RENAISSANCE
+## ---------------------------------
+
+TimePeriod.create(name: "Renaissance")
+
+Card.create(card_type: "Hero", name: "William Shakespeare", description: "an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's pre-eminent dramatist.", strength: 4, intelligence: 10, charisma: 6, picture_url: "http://en.wikipedia.org/wiki/Cobbe_portrait#mediaviewer/File:Cobbe_portrait_of_Shakespeare.jpg")
+
+# Card.create(card_type: "Hero", name: "Queen Elizabeth I", description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+## AGE OF ENLIGHTENMENT
+TimePeriod.create(name: "Age of Enlightenment")
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+## POST ENLIGHTENMENT EUROPE
+TimePeriod.create(name: "Post Enlightenment Europe")
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+## THE NEW WORLD
+TimePeriod.create(name: "The New World")
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+
+## INDUSTRIAL REVOLUTION
+TimePeriod.create(name: "Industrial Revolution")
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+## CONTEMPORARY
+TimePeriod.create(name: "Contemporary")
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+# Card.create(card_type: "Hero", name: , description: , strength: , intelligence: , charisma: , picture_url: )
+
+
+
+
+# 30.times do
+#   Card.create(card_type: "Hero", name: historical_figure_array.pop, description: Faker::Lorem.sentence, strength: rand(1..5), intelligence: rand(1..5), charisma: rand(1..5))
+# end
 
 
 deck1 = Deck.create(user_id: 1)
