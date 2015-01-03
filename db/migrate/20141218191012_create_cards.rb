a42f3d940518d554ec7bf37973f16e227a225980
Mason Norsworthy <masonnorsworthy@gmail.com>
2015-01-03 13:54:15 -0500

    initial commit

--- db/migrate/20141218191012_create_cards.rb
+++ db/migrate/20141218191012_create_cards.rb
@@ -1,13 +1,13 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.string :card_type, :null => false
-      t.string :name, :null => false
-      t.text :description, :null => false
-      t.integer :strength, :null => false
-      t.integer :intelligence, :null => false
-      t.integer :charisma, :null => false
-       t.references :time_period, index: true, :null => false
+      t.string   :card_type, :null => false
+      t.string   :name, :null => false
+      t.text     :description, :null => false
+      t.integer  :strength, :null => false
+      t.integer  :intelligence, :null => false
+      t.integer  :charisma, :null => false
+      t.references :time_period, index: true, :null => false
 
       t.timestamps
     end
