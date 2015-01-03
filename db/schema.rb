9ed9427da8e8163711556f7b6367182fdb8931ef
Matthew D. Huff <matthewhuff89@gmail.com>
2015-01-03 15:42:34 -0500

    Passed test for Create route for decks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231181947) do
+ActiveRecord::Schema.define(version: 20150103194704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20141231181947) do
     t.integer  "user_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "decks", ["user_id"], name: "index_decks_on_user_id", using: :btree
