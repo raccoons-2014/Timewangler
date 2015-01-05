94553f754d27c43099ebda2526d17c16dc2f6576
William Storey <wcstorey@gmail.com>
2015-01-04 21:53:56 -0500

    implemented additional refactor methods. tests working.

--- app/game/game_engine/game_runner.rb
+++ app/game/game_engine/game_runner.rb
@@ -1,52 +1,51 @@
 module GameEngine
   module GameRunner
-    def self.set_blank(player)
-        player.selection = []
+    # -------------------------------------------------
+    def self.reset_selections
+        @player_one.selection = []
+        @player_two.selection = []
     end
 
+    def self.subtract_points(player)
+        player.points -= @played_cards[1].max_stat
+    end
+
+    def self.add_points(player)
+        player.points += @played_cards[1].max_stat
+    end
+    # -------------------------------------------------
     def self.resolve_round(game)
-      attr_reader :played_cards
-      # This, once again, is some fairly hacky code. There is a massive amount of
-      # repetition and nested conditional statements here. The module might not even
-      # be necessary.
-      # ===========================================
-      # NEEDS TO BE REFACTORED BEFORE FINAL VERSION.
-      # ===========================================
-      player_one = game.player_one
-      player_two = game.player_two
-      played_cards = [player_one.selection.first, player_two.selection.first]
+
+      @player_one = game.player_one
+      @player_two = game.player_two
+      @played_cards = [@player_one.selection.first, @player_two.selection.first]
 
 
-      if played_cards.include?(nil)
-        if played_cards.all? {|x| x.nil?}
-          set_blank(player_one)
-          set_blank(player_two)
+      if @played_cards.include?(nil)
+        if @played_cards.all? {|x| x.nil?}
+          reset_selections
           # return nil
-        elsif played_cards[0].nil?
-          player_one.points -= played_cards[1].max_stat
-          player_two.points += played_cards[1].max_stat
-          player_one.selection = []
-          player_two.selection = []
+        elsif @played_cards[0].nil?
+          subtract_points(@player_one)
+          add_points(@player_two)
+          reset_selections
         else
-          player_two.points -= played_cards[1].max_stat
-          player_one.points += played_cards[1].max_stat
-          player_one.selection = []
-          player_two.selection = []
+          subtract_points(@player_two)
+          add_points(@player_one)
+          reset_selections
         end
-      elsif played_cards[0].max_stat > played_cards[1].max_stat
-        damage = (played_cards[0].max_stat - played_cards[1].max_stat).abs
+      elsif @played_cards[0].max_stat > @played_cards[1].max_stat
+        damage = (@played_cards[0].max_stat - @played_cards[1].max_stat).abs
 
-        player_two.points -= damage
-        player_one.points += damage
-        player_one.selection = []
-        player_two.selection = []
+        @player_two.points -= damage
+        @player_one.points += damage
+        reset_selections
       else
-        damage = (played_cards[0].max_stat - played_cards[1].max_stat).abs
+        damage = (@played_cards[0].max_stat - @played_cards[1].max_stat).abs
 
-        player_one.points -= damage
-        player_two.points += damage
-        player_one.selection = []
-        player_two.selection = []
+        @player_one.points -= damage
+        @player_two.points += damage
+        reset_selections
       end
     end
 
