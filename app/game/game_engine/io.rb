c89eadb54cfe04107e67082058374bd49c661f05
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-05 02:22:13 -0500

    fix bug in countdown timer on GameEngine::IO

--- app/game/game_engine/io.rb
+++ app/game/game_engine/io.rb
@@ -4,10 +4,14 @@ module GameEngine
 
       case player_id
       when game_state.player_one.id
-        return { player_hand: game_state.player_one.hand, player_points: game_state.player_one.points, round: game_state.round, phase: game_state.phase, time_remaining: GameEngine::GAME_RULES["#{game_state.phase}_time".to_sym] - (Time.now - game_state.time).to_i }
+        return { player_hand: game_state.player_one.hand, player_points: game_state.player_one.points, round: game_state.round, phase: game_state.phase, time_remaining: (Time.now - game_state.time).to_i }
       when game_state.player_two.id
-        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, time_remaining: GameEngine::GAME_RULES["#{game_state.phase}_time".to_sym] - (Time.now - game_state.time).to_i  }
+        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, time_remaining: (Time.now - game_state.time).to_i  }
       end
     end
+
+     def self.input_player_move(game_state, player_id, card_id)
+        game_state.target_player(player_id).play_cards([card_id])
+     end
   end
 end
