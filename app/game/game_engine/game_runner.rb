55430b63b82e775b7c408d1eaf40e33f382cada8
William Storey <wcstorey@gmail.com>
2015-01-04 23:22:12 -0500

    refactored to remove use of conditional statements, except for one .nil? check in outside method

--- app/game/game_engine/game_runner.rb
+++ app/game/game_engine/game_runner.rb
@@ -1,13 +1,13 @@
 module GameEngine
   module GameRunner
     # -------------------------------------------------
+
     def self.maxstat_nilcheck(player)
       maxstat_nilcheck = (player.selection.first.nil? ? 0 : player.selection.first.max_stat)
     end
 
     def self.damage
-      @max_stat_compare = maxstat_nilcheck(@player_one) - maxstat_nilcheck(@player_two)
-      @max_stat_compare.abs
+      maxstat_nilcheck(@player_one) - maxstat_nilcheck(@player_two)
     end
 
     def self.reset_selections
@@ -15,46 +15,18 @@ module GameEngine
         @player_two.selection = []
     end
 
-    def self.subtract_points(player)
-        player.points -= damage()
-    end
-
-    def self.add_points(player)
-        player.points += damage()
+    def self.calc_points
+        @player_one.points += damage
+        @player_two.points -= damage
     end
 
     # -------------------------------------------------
     def self.resolve_round(game)
-
       @player_one = game.player_one
       @player_two = game.player_two
-      @played_cards = [@player_one.selection.first, @player_two.selection.first]
 
-
-
-
-      if @played_cards.include?(nil)
-        if @played_cards.all? {|x| x.nil?}
-          reset_selections
-          # return nil
-        elsif @played_cards[0].nil?
-          subtract_points(@player_one)
-          add_points(@player_two)
-          reset_selections
-        else
-          subtract_points(@player_two)
-          add_points(@player_one)
-          reset_selections
-        end
-      elsif @played_cards[0].max_stat > @played_cards[1].max_stat
-        subtract_points(@player_two)
-        add_points(@player_one)
-        reset_selections
-      else
-        subtract_points(@player_one)
-        add_points(@player_two)
-        reset_selections
-      end
+      calc_points
+      reset_selections
     end
 
   end
