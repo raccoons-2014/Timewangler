936d554b9dff5e20551dfa02f957bd15ccb5c8bb
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2015-01-05 16:00:00 -0500

    changed name of the argument that resolve_round takes from game to game_state. in the game runner file.

--- app/game/game_engine/game_runner.rb
+++ app/game/game_engine/game_runner.rb
@@ -1,12 +1,12 @@
 module GameEngine
   module GameRunner
-    def self.resolve_round(game)
-      @player_one = game.player_one
-      @player_two = game.player_two
+    def self.resolve_round(game_state)
+      @player_one = game_state.player_one
+      @player_two = game_state.player_two
 
       calc_points
       reset_selections
-      game.phase = :won if game.won?
+      game_state.phase = :won if game_state.won?
     end
 
     def self.determine_maxstat(player)
