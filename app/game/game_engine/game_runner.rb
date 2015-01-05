e84fa327e4f15c2a58fb1a12af3a9be7f4cb7023
William Storey <wcstorey@gmail.com>
2015-01-04 23:46:52 -0500

    added explanatory comments and a more explanatory method name with determine_maxstat

--- app/game/game_engine/game_runner.rb
+++ app/game/game_engine/game_runner.rb
@@ -8,22 +8,24 @@ module GameEngine
       reset_selections
     end
 
-    def self.maxstat_nilcheck(player)
-      maxstat_nilcheck = (player.selection.first.nil? ? 0 : player.selection.first.max_stat)
+    def self.determine_maxstat(player)
+      player.selection.first.nil? ? 0 : player.selection.first.max_stat
     end
 
+    #if player one has a higher maxstat, the overall damage value returned is positive.  If player two has a higher maxstat, the overall damage value returned is negative.
     def self.damage
-      maxstat_nilcheck(@player_one) - maxstat_nilcheck(@player_two)
+      determine_maxstat(@player_one) - determine_maxstat(@player_two)
     end
 
-    def self.reset_selections
-        @player_one.selection = []
-        @player_two.selection = []
-    end
-
+    #Calc_points should work when the damage value returned from damage above is negative (when player two has a higher maxstat) or positive (when player one has a higher maxstat).  (For example, consider that the subtraction of a negative value adds a positive value and the addition of negative value subtracts that value.)
     def self.calc_points
         @player_one.points += damage
         @player_two.points -= damage
+    end
+
+    def self.reset_selections
+      @player_one.selection = []
+      @player_two.selection = []
     end
   end
 end
