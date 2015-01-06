625df4ea84214c362147af4dde88c09f7b7f7a88
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-06 00:20:08 -0500

    change Match constructor function to listen for window close

--- app/assets/javascripts/matching.js
+++ app/assets/javascripts/matching.js
@@ -5,69 +5,13 @@ function getGameId() {
   return urlArray[4];
 }
 
-// Checks to see if anyone has joined the match, logs in the console while waiting for a response
-// function checkMatch() {
-//   setTimeout(function() {
-//     console.log("Waiting for another player to join...");
-//     var gameId = getGameId()
-//
-//     $.ajax({
-//         url: "/games/" + getGameId() + "/status",
-//         method: "GET",
-//         dataType: "JSON",
-//         success: function(response) {
-//           console.log(response);
-//           if (response) {
-//
-//             $.ajax({
-//               url: response.url,
-//               method: "GET",
-//               success: function(response) {
-//                 window['onbeforeunload'] = undefined;
-//                 clearInterval(matchTimer);
-//                 window.location.href = '/games/' + gameId.toString();
-//               }
-//             })
-//           } else {
-//             checkMatch();
-//           }
-//         },
-//         error: function(response) {
-//           console.log("ERROR:");
-//           console.log(response);
-//         }
-//       });
-//
-//   }, 500)
-// }
-
-function cancelMatch() {
-  $.ajax({
-    url: "/games/" + getGameId(),
-    method: "DELETE"
-  })
-}
-
-// If a user has been searching too long (60 seconds), redirects to the profile path.
-function matchTimeout(numSeconds) {
-  var counter = 0;
-
-  matchTimer = setInterval(function() {
-                 counter ++;
-                 if (counter > numSeconds) {
-                   window.location.replace('/profile');
-                 }
-               }, 1000)
-}
-
-// Refactor
-
-
 function Match(id) {
+  var self = this;
+  window.onbeforeunload = function() { self.cancel() }
   this.id = id;
 }
 
-Match.prototype.listenForConnection = function() {
+Match.prototype.listenForConfirmation = function() {
   var match = this;
   setTimeout(function() { checkServer() }, 500);
 
