c31b8cb5f6518d2644fa7bfeb58465705b4e5608
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-05 23:05:50 -0500

    temporarily comment out old checkMatch() function

--- app/assets/javascripts/matching.js
+++ app/assets/javascripts/matching.js
@@ -6,40 +6,40 @@ function getGameId() {
 }
 
 // Checks to see if anyone has joined the match, logs in the console while waiting for a response
-function checkMatch() {
-  setTimeout(function() {
-    console.log("Waiting for another player to join...");
-    var gameId = getGameId()
-
-    $.ajax({
-        url: "/games/" + getGameId() + "/status",
-        method: "GET",
-        dataType: "JSON",
-        success: function(response) {
-          console.log(response);
-          if (response) {
-
-            $.ajax({
-              url: response.url,
-              method: "GET",
-              success: function(response) {
-                window['onbeforeunload'] = undefined;
-                clearInterval(matchTimer);
-                window.location.href = '/games/' + gameId.toString();
-              }
-            })
-          } else {
-            checkMatch();
-          }
-        },
-        error: function(response) {
-          console.log("ERROR:");
-          console.log(response);
-        }
-      });
-
-  }, 500)
-}
+// function checkMatch() {
+//   setTimeout(function() {
+//     console.log("Waiting for another player to join...");
+//     var gameId = getGameId()
+//
+//     $.ajax({
+//         url: "/games/" + getGameId() + "/status",
+//         method: "GET",
+//         dataType: "JSON",
+//         success: function(response) {
+//           console.log(response);
+//           if (response) {
+//
+//             $.ajax({
+//               url: response.url,
+//               method: "GET",
+//               success: function(response) {
+//                 window['onbeforeunload'] = undefined;
+//                 clearInterval(matchTimer);
+//                 window.location.href = '/games/' + gameId.toString();
+//               }
+//             })
+//           } else {
+//             checkMatch();
+//           }
+//         },
+//         error: function(response) {
+//           console.log("ERROR:");
+//           console.log(response);
+//         }
+//       });
+//
+//   }, 500)
+// }
 
 function cancelMatch() {
   $.ajax({
