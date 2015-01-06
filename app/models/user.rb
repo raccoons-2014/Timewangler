a459edf771fa842fe6ac43a2734dc01f9a9e74c8
Matthew D. Huff <matthewhuff89@gmail.com>
2015-01-06 15:00:46 -0500

    Created methods to count a user's wins, losses, and calculate the win-loss ratio

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ActiveRecord::Base
 
   validates :password, :presence => true, :length => { :within => 4..40 }
   validates :username, :presence => true, :length => { :within => 4..40 }
+
+  def count_wins
+   wins = Game.where(winner_id: self.id).count
+  end
+
+  def count_losses
+    losses = self.games.count - count_wins
+  end
+
+  def win_loss_ratio
+    count_wins / count_losses
+  end
+
+  def last_3_games
+    games = self.games
+  end
+
 end
