932e44224fcb5320a2aad0f3f8d2956cfacfd376
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2015-01-06 18:17:52 -0500

    made the GameEngine::Cache module's methods take 2 arguments so that we can inject the depency of 'caching_interface' into our module so that Rails.cache will never be referenced in our gem.

--- app/game/game_engine/cache.rb
+++ app/game/game_engine/cache.rb
@@ -1,15 +1,15 @@
 module GameEngine
   module Cache
-    def self.save_game_state(game)
-      Rails.cache.write "#{game.id}#{game.player_one.id}#{game.player_two.id}", game
+    def self.save_game_state(game, caching_interface)
+      caching_interface.write "#{game.id}#{game.player_one.id}#{game.player_two.id}", game
     end
 
-    def self.fetch_game_state(game_data)
-      Rails.cache.read "#{game_data.id}#{game_data.player_one.id}#{game_data.player_two.id}"
+    def self.fetch_game_state(game_data, caching_interface)
+      caching_interface.read "#{game_data.id}#{game_data.player_one.id}#{game_data.player_two.id}"
     end
 
-    def self.clear_game_state(game_data)
-      Rails.cache.delete "#{game_data.id}#{game_data.player_one.id}#{game_data.player_two.id}"
+    def self.clear_game_state(game_data, caching_interface)
+      caching_interface.delete "#{game_data.id}#{game_data.player_one.id}#{game_data.player_two.id}"
     end
   end
 end
