a06e73b35fa315f2eb26dadf995ee1303c874934
William Storey <wcstorey@gmail.com>
2015-01-05 22:22:05 -0500

    continued refactoring. tests passed

--- app/game/game_engine/controller.rb
+++ app/game/game_engine/controller.rb
@@ -5,6 +5,14 @@ module GameEngine
       GameEngine::Cache.save_game_state(game_state)
     end
 
+    def self.fetch_game_state(game_data)
+      GameEngine::Cache.fetch_game_state(game_data)
+    end
+
+    def self.update_game_state_time(game_state)
+      game_state.time = Time.now
+    end
+
     def self.output_player_data(game_state, player_id)
       GameEngine::IO.output_player_data(game_state, player_id)
     end
@@ -18,20 +26,25 @@ module GameEngine
       # This method is responsible for switching phases inside of the GameEngine::Game
       # class when passed the ActiveRecord model of the game and the player_id for the
       # client that's making the request.
-      game_state = GameEngine::Cache.fetch_game_state(game_data)
+      game_state = fetch_game_state(game_data)
       current_time = Time.now
 
       current_player = game_state.target_player(player_id.to_i)
       player_one = game_state.player_one
       player_two = game_state.player_two
 
+
       if game_state.phase == :setup
         if current_time - game_state.time >= GAME_RULES[:setup_time]
           game_state.phase = :move
+
           game_state.time = Time.now
 
           GameEngine::GameResolver.deal_cards(game_state)
           GameEngine::Cache.save_game_state(game_state)
+
+
+          update_game_state_time(game_state)
 
           game_state.deal_cards
 
@@ -58,7 +71,7 @@ module GameEngine
 
         if selection_made?(player_one) && selection_made?(player_two)
           game_state.phase = :resolution
-          game_state.time = Time.now
+          update_game_state_time(game_state)
           save_game_state(game_state)
           output_player_data(game_state, player_id)
         end
@@ -80,7 +93,7 @@ module GameEngine
     end
 
     def self.get_player_move(game_data, player_id, card_id)
-      game_state = GameEngine::Cache.fetch_game_state(game_data)
+      game_state = fetch_game_state(game_data)
       if !selection_made?(current_player) && game_state.phase == :move
         GameEngine::IO.input_player_move(game_state, player_id, card_id)
         save_game_state(game_state)
