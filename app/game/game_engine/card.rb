3c19bcb46df830bb012a5c8e0bb3ef218b05e9f1
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-06 11:56:08 -0500

    add effect properties to the GameEngine::Card object

--- app/game/game_engine/card.rb
+++ app/game/game_engine/card.rb
@@ -1,12 +1,14 @@
 module GameEngine
   class Card
-    attr_reader :id, :card_type, :name, :description, :strength, :intelligence, :charisma, :time_period
+    attr_reader :id, :card_type, :name, :description, :strength, :intelligence, :charisma, :time_period, :effect_description, :effect_dsl
 
     def initialize(card_data)
       @id = card_data.id
       @type = card_data.card_type
       @name = card_data.name
       @description = card_data.description
+      @effect_description = card_data.description
+      @effect_dsl = card_data.effect_dsl
       @strength = card_data.strength
       @intelligence = card_data.intelligence
       @charisma = card_data.charisma
