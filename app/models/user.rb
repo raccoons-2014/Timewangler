fbad871dbda3873f921b8f1645cee5d6d6528b3d
Matthew D. Huff <matthewhuff89@gmail.com>
2015-01-06 16:23:25 -0500

    Changed last_3_games to last_20_games

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,14 +26,5 @@ class User < ActiveRecord::Base
 
   def last_20_games
     games = self.games.where.not(winner_id: nil).order(created_at: :asc).limit(20)
-    losses = self.games.count - count_wins
-  end
-
-  def win_loss_ratio
-    if count_losses == 0 && count_wins > 0
-      return count_wins
-    elsif count_losses == 0 && count_wins == 0
-      return "No completed games."
-    end
   end
 end
