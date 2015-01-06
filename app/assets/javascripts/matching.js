d4f0d66b750b2439745d9145494746c2127eb197
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-05 23:06:28 -0500

    create Match object to contain related data, add a listenForConnection() function to the Match object

--- app/assets/javascripts/matching.js
+++ app/assets/javascripts/matching.js
@@ -59,3 +59,38 @@ function matchTimeout(numSeconds) {
                  }
                }, 1000)
 }
+
+// Refactor
+
+
+function Match(id) {
+  this.id = id;
+}
+
+Match.prototype.listenForConnection = function() {
+  var match = this;
+  setTimeout(function() { checkServer() }, 500);
+
+  function checkServer() {
+    console.log("Waiting for another player to join...");
+
+    $.ajax({
+        url: "/games/" + match.id + "/status",
+        method: "GET",
+        dataType: "JSON"
+      }).done(function(response) {
+        console.log(response);
+
+        if (response) {
+          $.ajax({
+            url: response.url,
+            method: "GET"}).done(function(response) {
+              clearInterval(matchTimer);
+              window.location.href = '/games/' + match.id.toString();
+          });
+        } else {
+          match.listenForConnection();
+        }
+    });
+  }
+}
