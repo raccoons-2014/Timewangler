688afd29a3df214a7bb6570e8b79797fe5ac5f6b
Matthew D. Huff <matthewhuff89@gmail.com>
2015-01-05 22:52:22 -0500

    Wrote tests for IO Module, minor refactor

--- app/game/game_engine/io.rb
+++ app/game/game_engine/io.rb
@@ -1,12 +1,13 @@
 module GameEngine
   module IO
     def self.output_player_data(game_state, player_id)
+      time_remaining = (Time.now - game_state.time).to_i
 
       case player_id
       when game_state.player_one.id
-        return { player_hand: game_state.player_one.hand, player_points: game_state.player_one.points, round: game_state.round, phase: game_state.phase, time_remaining: (Time.now - game_state.time).to_i }
+        return { player_hand: game_state.player_one.hand, player_points: game_state.player_one.points, round: game_state.round, phase: game_state.phase, time_remaining: time_remaining }
       when game_state.player_two.id
-        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, time_remaining: (Time.now - game_state.time).to_i  }
+        return { player_hand: game_state.player_two.hand, player_points: game_state.player_two.points, round: game_state.round, phase: game_state.phase, time_remaining: time_remaining  }
       end
     end
 
