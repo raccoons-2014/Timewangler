e67659795d612fcebbfd4b94767dd41be1ba4326
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2015-01-06 19:12:07 -0500

    DRYed cache_spec up a bit

--- spec/game/cache_spec.rb
+++ spec/game/cache_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "GameEngine::Cache" do
+  caching_interface = Rails.cache
+
   let(:game_data) { game_data = create(:game) }
 
   let(:game) do
@@ -12,24 +14,27 @@ describe "GameEngine::Cache" do
   end
 
   describe '#save_game_state' do
+
     it 'does not return false after saving game data' do
-      expect(GameEngine::Cache.save_game_state(game, Rails.cache)).to_not eq false
+      expect(GameEngine::Cache.save_game_state(game, caching_interface)).to_not eq false
     end
   end
 
   describe '#fetch_game_state' do
+
     it 'returns the correct object' do
-      GameEngine::Cache.save_game_state(game, Rails.cache)
-      expect(GameEngine::Cache.fetch_game_state(game_data, Rails.cache).id).to eq game.id
+      GameEngine::Cache.save_game_state(game, caching_interface)
+      expect(GameEngine::Cache.fetch_game_state(game_data, caching_interface).id).to eq game.id
     end
   end
 
   describe '#clear_game_state' do
+
     it 'deletes the correct object from the cache' do
-      GameEngine::Cache.save_game_state(game, Rails.cache)
-      expect(GameEngine::Cache.fetch_game_state(game_data, Rails.cache)).to_not eq nil
-      GameEngine::Cache.clear_game_state(game_data, Rails.cache)
-      expect(GameEngine::Cache.fetch_game_state(game_data, Rails.cache)).to eq nil
+      GameEngine::Cache.save_game_state(game, caching_interface)
+      expect(GameEngine::Cache.fetch_game_state(game_data, caching_interface)).to_not eq nil
+      GameEngine::Cache.clear_game_state(game_data, caching_interface)
+      expect(GameEngine::Cache.fetch_game_state(game_data, caching_interface)).to eq nil
     end
   end
 end
