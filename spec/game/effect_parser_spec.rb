53383f659bd98c409b3ccfe29c5336887b390e5b
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-01-06 21:56:15 -0500

    add more tests to check all types of arithmetic in the #resolve_target_modifier_method

--- spec/game/effect_parser_spec.rb
+++ spec/game/effect_parser_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'GameEngine::EffectParser' do
   describe 'private methods' do
+    before(:each) { @fake_card = GameEngine::Card.new(create(:card)) }
     describe '#resolve_target_player' do
       #..
     end
@@ -15,31 +16,69 @@ describe 'GameEngine::EffectParser' do
     end
 
     describe '#resolve_target_modifier' do
-      let(:fake_card) { GameEngine::Card.new(create(:card)) }
 
-      it 'should deduct one strength from the target card' do
-        dsl_string = '[player] (hand all) |strength| {-1}'
-        old_strength = fake_card.strength
+      it 'should deduct 1 strength from the target card' do
+        dsl_string = '[player] (selection all) |strength| {-1}'
+        old_strength = @fake_card.strength
         GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
-        GameEngine::EffectParser.send(:resolve_target_modifier, fake_card, 'strength')
-        expect(fake_card.strength).to eq old_strength - 1
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'strength')
+        expect(@fake_card.strength).to eq old_strength - 1
       end
 
-      it 'should deduct one intelligence from the target card' do
-        dsl_string = '[player] (hand all) |intelligence| {-1}'
-        old_intelligence = fake_card.intelligence
+      it 'should deduct 1 intelligence from the target card' do
+        dsl_string = '[player] (selection all) |intelligence| {-1}'
+        old_intelligence = @fake_card.intelligence
         GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
-        GameEngine::EffectParser.send(:resolve_target_modifier, fake_card, 'intelligence')
-        expect(fake_card.intelligence).to eq old_intelligence - 1
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'intelligence')
+        expect(@fake_card.intelligence).to eq old_intelligence - 1
       end
 
-      it 'should deduct one charisma from the target card' do
-        dsl_string = '[player] (hand all) |intelligence| {-1}'
-        old_charisma = fake_card.charisma
+      it 'should add 5 intelligence to target card' do
+        dsl_string = '[player] (selection all) |intelligence| {+5}'
+        old_intelligence = @fake_card.intelligence
         GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
-        GameEngine::EffectParser.send(:resolve_target_modifier, fake_card, 'charisma')
-        expect(fake_card.charisma).to eq old_charisma - 1
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'intelligence')
+        expect(@fake_card.intelligence).to eq old_intelligence + 5
       end
+
+      it 'should deduct 1 charisma from the target card' do
+        dsl_string = '[player] (selection all) |charisma| {+3}'
+        old_charisma = @fake_card.charisma
+        GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'charisma')
+        expect(@fake_card.charisma).to eq old_charisma + 3
+      end
+
+      it 'should multiply the strength of target card by 2' do
+        dsl_string = '[player] (selection all) |strength| {*2}'
+        old_strength = @fake_card.strength
+        GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'strength')
+        expect(@fake_card.strength).to eq old_strength * 2
+      end
+
+      it 'should divide the strength of target card by 3' do
+        dsl_string = '[player] (selection all) |strength| {/3}'
+        old_strength = @fake_card.strength
+        GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'strength')
+        expect(@fake_card.strength).to eq old_strength / 3
+      end
+
+
+      it 'should only modify the attribute it is supposed to' do
+        dsl_string = '[player] (selection all) |charisma| {-1}'
+        original_strength = @fake_card.strength
+        original_charisma = @fake_card.charisma
+        original_intelligence = @fake_card.intelligence
+        GameEngine::EffectParser.instance_variable_set(:@dsl_string, dsl_string)
+        GameEngine::EffectParser.send(:resolve_target_modifier, @fake_card, 'charisma')
+        expect(@fake_card.charisma).to eq original_charisma - 1
+        expect(@fake_card.strength).to eq original_strength
+        expect(@fake_card.intelligence).to eq original_intelligence
+      end
+
+
 
 
     end
